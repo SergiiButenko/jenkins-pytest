6b615e8cdc582104920563d8979d1e6fb42bd2c7
Sergii Butenko <sergii.butenko@globallogic.com>
2025-08-21 19:57:24 +0200

    addde more tests

--- test_first_test.py
+++ test_first_test.py
@@ -1,5 +1,14 @@
 def test_math_add():
     assert 1+1 == 2
 
+
 def test_math_minus():
     assert 1 - 1 == 0
+
+
+def test_math_multiply():
+    assert 1 * 3 == 3
+
+
+def test_math_add_negative():
+    assert 1 + 1 == 3
